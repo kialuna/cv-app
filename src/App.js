3c822545abcde37dc7e6868133efc96cffe38419
kialuna <77881537+kialuna@users.noreply.github.com>
2024-01-06 19:15:20 +0000

    Started implementing react-pro-sidebar

--- src/App.js
+++ src/App.js
@@ -1,95 +1,33 @@
 
 import { useState } from 'react';
+import Board  from "./xoxo.js"
+import { Home, Folder, Message } from '@mui/icons-material';
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function Square({value, onSquareClick}){
-  // const [value, setValue] = useState(null);
-  // function handleClick(){
-  //   console.log('clicked')
-  //   setValue('X')
-  // }
-  return (
-  <button 
-    className='square'
-    onClick={onSquareClick}>
-      {value}
-      </button>)
+export default function App() {
+const[collapsed, setCollapsed] = useState(false);
+
+
+const handleToggleSidebar = () => {
+    setCollapsed(!collapsed);
+};
+
+    return(
+        <>
+         <Sidebar collapsed={collapsed}>
+            <Menu>
+                <MenuItem icon={<Home/>}>Board</MenuItem>
+            </Menu>
+            <button onClick={handleToggleSidebar}>Toggle</button>
+        </Sidebar>    
+        <Board/>   
+        </>
+
+    // <Router>
+    //     <Switch>
+    //         <Route exact path="/" component={Board}/>
+    //     </Switch>
+    // </Router>
+    )
 }
-
-export default function Board() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  const [xISNext, setXIsNext] = useState(true);
-
-  function handleClick(i){
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xISNext){
-      nextSquares[i]="X";
-    } else {
-      nextSquares[i]="O";
-    }
-    setSquares(nextSquares)
-    setXIsNext(!xISNext);
-
-  }
-
-  const winner =calculateWinner(squares);
-  let status; 
-  if(winner){
-    status='Winner is:' + winner
-  } else {
-    status = 'Next player:' + (xISNext ? 'X' : 'O');
-  }
-
-
-  return (
-    <>
-      <h3>Welcome to noughts and crosses!</h3>
-      <p>Get started by clicking a box</p>
-    <div className='status'>{status}</div>
-      <div className="board-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-  </>
-  );
-}
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-
-
-console.log()
