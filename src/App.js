0663c3f5b0119ac9b128b021efb4f96444aebbe8
kialuna <77881537+kialuna@users.noreply.github.com>
2024-01-07 17:57:12 +0000

    Sidebar working with router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import { useState } from 'react';
 import Board  from "./xoxo.js"
-import { Home, Folder, Message } from '@mui/icons-material';
+import { Home, VideogameAssetOutlined  } from '@mui/icons-material';
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
 const[collapsed, setCollapsed] = useState(false);
@@ -13,16 +13,42 @@ const handleToggleSidebar = () => {
     setCollapsed(!collapsed);
 };
 
+
+const HomePage = () => {
+    return (
+      <>
+        <h1 className="header">Welcome to Kia's React app!</h1>
+        <h3>This is a great place</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+      </>
+    );
+  };
+
     return(
-        <>
+        <div style={{ display: "flex", height: "100vh" }}>
          <Sidebar collapsed={collapsed}>
             <Menu>
-                <MenuItem icon={<Home/>}>Board</MenuItem>
+                <MenuItem 
+                component = {<Link to="home" className="link" />}
+                icon={<Home/>}>
+                    Home
+                </MenuItem>
+                <MenuItem
+                component={<Link to="tictactoe" className='link'/>}
+                icon= {<VideogameAssetOutlined/>} >
+                    Games
+                </MenuItem>
+
             </Menu>
             <button onClick={handleToggleSidebar}>Toggle</button>
         </Sidebar>    
-        <Board/>   
-        </>
+        <section id="main-section">
+        <Routes>
+          <Route path="home" element={<HomePage />} />
+          <Route path="tictactoe" element={<Board />} />
+        </Routes>
+      </section>
+        </div>
 
     // <Router>
     //     <Switch>
